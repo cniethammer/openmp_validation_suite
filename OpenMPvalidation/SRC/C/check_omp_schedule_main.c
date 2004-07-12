ea209079b6569a310c17aaa151ddb2dfa9ad1297
Christoph Niethammer <niethammer@hlrs.de>
2004-07-12 11:42:21 +0000

    Added checks for loops schedule(static),schedule(dynamic) and schedule(guided)

--- OpenMPvalidation/SRC/C/check_omp_schedule_main.c
+++ OpenMPvalidation/SRC/C/check_omp_schedule_main.c
@@ -23,13 +23,48 @@ int main(int argc,char** argv){
   }
 #endif
 
-  printf("omp in schedule...");
-if(check_for_schedule())
-{
-printf("passed\n");
-}
-else 
-{printf("failed\n");}
+  	/* printf("omp in schedule ... ");
+	if(check_for_schedule())
+	{
+		printf("passed\n");
+	}
+	else 
+	{
+		printf("failed\n");
+	}
+	*/
+	printf(" omp in schedule static ... ");
+  	if(check_for_schedule_static())
+  	{
+    	printf("passed\n");
+  	} 
+	else 
+	{
+    	printf("failed\n");
+  	}
+
+	printf(" omp in schedule dynamic ... ");
+  	if(check_for_schedule_dynamic())
+  	{
+    	printf("passed\n");
+  	} 
+	else 
+	{
+    	printf("failed\n");
+  	}
+
+	printf(" omp in schedule guided ... ");
+  	if(check_for_schedule_guided())
+  	{
+    	printf("passed\n");
+  	} 
+	else 
+	{
+    	printf("failed\n");
+  	}
+
+
 return 0;
+
 }
 
