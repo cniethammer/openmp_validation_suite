616a201d7210255ad224652cb9cd58dc3c2db2e3
Matthias Mueller <mueller@hlrs.de>
2004-08-30 11:55:53 +0000

    Improved version: now each chunk is identified, independent of schedule and chunksize. Instead of a token we wait until another thread has touched a higher iteration count.

--- OpenMPvalidation/SRC/C/omp_check_chunksize.c
+++ OpenMPvalidation/SRC/C/omp_check_chunksize.c
@@ -1,87 +1,109 @@
 #include <stdio.h>
 #include <time.h>
 #include <omp.h>
+#include <unistd.h>
 
 #include <time.h>
 
 #define NUMBER_OF_THREADS 5
-#define MAX_SIZE 500
-#define MAX_TIME 10000000
-#define SLEEPTIME 0.001
+#define MAX_SIZE 8
+#define MAX_TIME 10
+#define SLEEPTIME 1
 
 int main()
 {
-	int tid,nexttid,threads;
-	static const int chunk_size = 10;
-	int tids[MAX_SIZE + chunk_size];
+	int threads;
+	int tids[MAX_SIZE+1];
 	int i,m,tmp;
-	double count = 0.; 
-	int got[20];
-	int notout = 1;
-	const int chunksize = 10;
-	
-	for(i=0;i<NUMBER_OF_THREADS;++i)
-	{
-		got[i]=1;
-	}
 
-	got[0]=0;
+	int notout = 1;
+	int maxiter=0;
+	
 #pragma omp parallel
 	{
-threads = omp_get_num_threads();
+#pragma omp single
+	    {
+		threads = omp_get_num_threads();
+	    }
 	}	
 
 	if(threads < 2)
 	{
-		printf("Cannot create enougth threads for this test.\n");
+		printf("This test only works with at least two threads .\n");
 		return 0;
 	}
-#pragma omp parallel shared(tids) private (tid,nexttid)
+
+/* Now the real parallel work:  
+
+   Each thread will start immediately with the first chunk.
+
+*/
+
+#pragma omp parallel shared(tids) 
 	{
-		double count = 0.;
+		int count = 0.;
+		int tid;
 		tid = omp_get_thread_num();
-		nexttid = (tid==0)? omp_get_num_threads()-1:tid-1;
 		
-#pragma omp for nowait schedule(guided,1)
+#pragma omp for nowait schedule(dynamic,1)
 		for(i=0;i<MAX_SIZE;++i)
 		{
-			got[tid]=1;
-#pragma omp flush(got)
-			/*printf("Thread Nr. %d working once\n",tid);*/
-			tids[i]=tid;
-			/*printf("%d Thread Nr. %d starting Thread Nr. %d\n",i,tid,nexttid);*/
-			got[nexttid]=0;
-#pragma omp flush(got)
-			while(notout && count < MAX_TIME && got[tid])
+			printf(" notout=%d, count= %d\n",notout,count);
+			count=0;
+			#pragma omp flush(maxiter)
+			if(i>maxiter){
+			    #pragma omp critical
+			    {
+				maxiter=i;
+			    }
+			}
+
+			/* if it is not our turn we wait 
+                            a) until another thread executed an iteration 
+                               with a higher iteration count
+                            b) we are at the end of the loop (first thread finished                           and set notout=0 OR
+                            c) timeout arrived */ 
+
+			while(notout && (count < MAX_TIME) && (maxiter==i))
 			{
-#pragma omp flush(got)
+			printf("Thread Nr. %d sleeping\n",tid);
+#pragma omp flush(maxiter,notout)
 				sleep(SLEEPTIME);
 				count+=SLEEPTIME;
 			}
-			/*printf("Thread Nr. %d awake again\n",tid);*/
-		}
+			printf("Thread Nr. %d working once\n",tid);
+			tids[i]=tid;
+		} /*end omp for*/
 		notout = 0;
-#pragma omp for schedule(dynamic,1)
-		for(m=0;m<NUMBER_OF_THREADS;++m)
-		{
-			got[m]=0;
-		}
-	}
+#pragma omp flush(notout)
+	}/* end omp parallel*/
+
 	m=1;
 	tmp=tids[0];
-	
-	for(i=1;i<MAX_SIZE;++i)
 	{
+	    int global_chunknr=0;
+	    int local_chunknr[NUMBER_OF_THREADS];
+
+	    for(i=0;i<NUMBER_OF_THREADS;i++)
+		local_chunknr[i]=0;
+	    
+	    tids[MAX_SIZE]=-1;
+	    printf("# global_chunknr thread local_chunknr chunksize\n"); 
+	    for(i=1;i<=MAX_SIZE;++i)
+	    {
 		if(tmp==tids[i])
 		{
-			m++;
+		    m++;
 		}
 		else
 		{
-			tmp=tids[i];
-			printf("%d\n",m);
-			m=1;
+		    printf("%d %d %d %d\n",global_chunknr,tmp,local_chunknr[tmp],m);
+		    global_chunknr++;
+		    local_chunknr[tmp]++;
+		    tmp=tids[i];
+		    m=1;
 		}
+	    }
 	}
 	return 0;
 }
