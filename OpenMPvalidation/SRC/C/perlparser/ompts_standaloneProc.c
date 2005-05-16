0b8f915519766dc811b23e161f59a1b6d5ebbc37
Christoph Niethammer <niethammer@hlrs.de>
2005-05-16 15:27:53 +0000

    Made some modifications to use the new subroutines for the ompts_parser.pl script.

--- OpenMPvalidation/SRC/C/perlparser/ompts_standaloneProc.c
+++ OpenMPvalidation/SRC/C/perlparser/ompts_standaloneProc.c
@@ -7,25 +7,25 @@ int main()
 	int failed=0, success=0;
 	int N=REPETITIONS;
 	static FILE * logFile;
-	static const char * logFileName = "<testfunctionname>.log";;
+	static const char * logFileName = "<testfunctionname></testfunctionname>.log";
 
 
-	logFile = fopen(logFileName,"a");
+	logFile = fopen(logFileName,"w+");
 
 	printf("######## OpenMP Validation Suite V 0.94 ######\n");
 	printf("## Repetitions: %3d                       ####\n",N);
 	printf("## Loop Count : %6d                    ####\n",LOOPCOUNT);
 	printf("##############################################\n");
-	printf("Testing <directive>\n\n");
+	printf("Testing <directive></directive>\n\n");
 
 	fprintf(logFile,"######## OpenMP Validation Suite V 0.94 ######\n");
 	fprintf(logFile,"## Repetitions: %3d                       ####\n",N);
 	fprintf(logFile,"## Loop Count : %6d                    ####\n",LOOPCOUNT);
 	fprintf(logFile,"##############################################\n");
-	fprintf(logFile,"Testing <directive>\n\n");
+	fprintf(logFile,"Testing <directive></directive>\n\n");
 
 	for(i=0;i<N;i++){
-		if(<testfunctionname>(logFile)){
+		if(<testfunctionname></testfunctionname>(logFile)){
 			fprintf(logFile,"Test succesfull.\n");
 			success++;
 		}
@@ -37,12 +37,12 @@ int main()
 	}
 
     if(failed==0){
-		fprintf(logFile,"Directiv worked without errors.\n");
+		fprintf(logFile,"\nDirectiv worked without errors.\n");
 		printf("Directiv worked without errors.\n");
 		result=0;
 	}
 	else{
-		fprintf(logFile,"Directive failed the test %i times out of %i. %i were successful\n",failed,N,success);
+		fprintf(logFile,"\nDirective failed the test %i times out of %i. %i were successful\n",failed,N,success);
 		printf("Directive failed the test %i times out of %i. %i were successful\n",failed,N,success);
 		result=failed;
 	}
