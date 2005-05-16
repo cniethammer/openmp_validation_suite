787a1ec6eb90dc8377e49251fecb84c850dff6d6
Christoph Niethammer <niethammer@hlrs.de>
2005-05-16 14:34:56 +0000

    Added comments in output like what options were used when running the ompts_makeHeader.pl script.

--- OpenMPvalidation/SRC/C/perlparser/ompts_makeHeader.pl
+++ OpenMPvalidation/SRC/C/perlparser/ompts_makeHeader.pl
@@ -1,28 +1,54 @@
 #!/usr/bin/perl -w
 
-# ompts_makeHeader [options] [dir]
+# ompts_makeHeader [options] [dirs]
 #
 # Creats the headerfile for the OpenMP-Testsuite out of the templatefiles 
 # witch are in the default/explicitely specified dir and the settings in the
 # ompts.conf file in the main directory.
 #
-# dir:
+# dirs:
 # 	Specifies the dir with the templatefiles. Default is "./templates"
 # 
 # options:
-# -i: Include other Headerfile. The files to be included must be specified
-# after setting this option.
+# -f=FILENAME: Using file FILENAME as configfile instead of the default (ompts.conf)
+# -i=FILENAME: Include other Headerfile. The files to be included must be specified
+# after setting this option. (Not implemented yet.)
 # 
 # -o=FILENAME: outputfilename (default is "omp_testsuite.h")
 
+$headerfile = "\/\* Global headerfile of the OpenMP Testsuite \*\/\n\n\/\* This file was created with the ompts_makeHeder.pl script using the following opions:\ *\/\n\/\* ";
+if(@ARGV > 0)
+{
+	foreach $opt (@ARGV)
+	{
+		$headerfile .= "$opt ";
+	}
+}
+else
+{
+	$headerfile .= "No options were specified";
+}
 
-$outfile = "omp_testsuite.h";
-$templatedir = "./templates";
-$configfile = "ompts.conf";
-@includefiles;
+$headerfile .=" \*\/\n\n\n";
 
-# generating the includeguard:
-$headerfile = "\#ifndef OMP_TESTSUITE_H\n\#define OMP_TESTSUITE_H\n\n";
+use Getopt::Long;
+GetOptions("-o=s" => \$outfile, "-f=s" =>\$configfile);
+
+if(!$outfile){
+	$outfile = "omp_testsuite.h";	# setting default value for the headerfile
+}
+
+if(!$configfile) {	
+	$configfile = "ompts.conf"; 	# setting default value for the configfile 
+}	
+
+$templatedir = "./templates";	# dir holding the templates
+
+#@includefiles;					# list holing extra includefiles specified by the user 
+
+
+# generating the head of the includeguard:
+$headerfile .= "\#ifndef OMP_TESTSUITE_H\n\#define OMP_TESTSUITE_H\n\n";
 
 # inserting general settings out of ompts.conf:
 open(OMPTS_CONF,$configfile) or die "Could not open the global config file $configfile.";
