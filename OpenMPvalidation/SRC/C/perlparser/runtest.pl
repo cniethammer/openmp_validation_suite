f9e97e6d64fda12e69f7551ed16b4cec76ecd214
Christoph Niethammer <niethammer@hlrs.de>
2005-08-16 11:18:33 +0000

    Added support for checks with different counts of threads.

--- OpenMPvalidation/SRC/C/perlparser/runtest.pl
+++ OpenMPvalidation/SRC/C/perlparser/runtest.pl
@@ -1,75 +1,132 @@
 #!/usr/bin/env perl
 
-$testfile="testlist.txt";
-$numthreads=2;
-
-
+# runtest [options] FILENAME
 #
-# Read the file $testfile. Each line contains a test. 
-# Convert template to test and crosstest
+# Read the file FILENAME. Each line contains a test. 
+# Convert template to test and crosstest. 
+# If possilble generate orphaned testversions, too.
 # Use make to compile the test
 #
-$testfile=$ARGV[0];
+# Options:
+# -norphan		do not generate orphaned test versions
+# -maxthread=COUNT	run tests with a threadnumber from 2 up to COUNT
+# -d=DIR		specify the directory containing the templates
+#			default is templates
+# 
+
+$results = "results.txt";
+$dir = "templates";
+
+# Using Getopt::long to extract the programm options
+use Getopt::Long;
+
+# Getting given options
+GetOptions("-norphan" => \$norphan,"-maxthread=i" => \$maxthread, "-d=s" => \$dir, "-f!");
+$testfile = $ARGV[0];
+
+# Set maxthread if it was not specified with the program arguments
+if(!($maxthread)){
+    $maxthread = 2;
+}
+# Checking if given testfile exists
+die "The specifeid testlist does not exist." if(!(-e $testfile));
+
+# printing some Information
+print "Running tests with maximal $maxthread threads.\n";
+print "Using testlist $testfile for input.\n";
+print "Using dir $dir to search testtemplates.\n";
+
+# generating an up to date header file
+print "Generating headerfile ...\n";
+$cmd = "./ompts_makeHeader.pl $dir";
+system($cmd);
+
+print "Reading testlist and runtests ...\n";
+
 open(TEST,$testfile) or die "Error: Could not open  $testfile\n";
+open(RESULTS,">$results") or die "Error: Could not create  $results\n";
+
+print RESULTS "\\ Nmer of Threads\t";
+for($j=2; $j <= $maxthread; $j++){
+    print RESULTS "$j\t\t\t\t";
+}
+print RESULTS "\nTested Directive";
+for($j=2; $j <= $maxthread; $j++){
+    print RESULTS "\tt\tct\tot\toct";
+}
+print RESULTS "\n";
+
+    
 while(<TEST>){
     $testname = $_;
     chomp($testname);
-    
-    for($i=0; $i<2; $i++){
-	# Create templates:
-	$template = "templates/".$testname.".tpl";
+    print RESULTS "$testname\t";
 
-	$cmd="grep -q ompts:orphan ".$template;
-	$orphanedtest=system($cmd);
-	if( ($i==1) && ($orphanedtest==0) ){
-	    $orphanflag=" -orphan";
-	    $orphanname=" orphaned ";
-	} else {
-	    $orphanflag="";
-	    $orphanname="";
-	}	    
+    for($numthreads = 2; $numthreads <= $maxthread; $numthreads++){
+	print "Using $numthreads threads  ";
+	for($i=0; $i<2; $i++){
+	    # Create templates:
+	    $template = $dir."/".$testname.".tpl";
 
-	if( ($i==0) || ($orphanedtest==0) ){
-	    $cmd="./ompts_parser.pl ".$template." -test".$orphanflag;
-	    system($cmd);
-	    $cmd="./ompts_parser.pl ".$template." -crosstest".$orphanflag;
-	    system($cmd);
-	    
-	    # Compile:
-	    $cmd="make test_".$testname." >> compile.log";
-	    system($cmd);
-	    $cmd="make crosstest_".$testname." >> compile.log";
-	    system($cmd);
-	    
-	    # Run the tests:
-	    $cmd = "OMP_NUM_THREADS=".$numthreads."; ./test_".$testname."> test_".$testname.".out";
-	    $exit_status = system($cmd);
-	    if ($exit_status){
-		print $testname.$orphanname." failed !!! " ;
-		$failed = $exit_status >> 8;
+	    $cmd="grep -q ompts:orphan ".$template;
+	    $orphanedtest=system($cmd);
+	    if( ($i==1) && ($orphanedtest==0) && !($norphan) ){
+		$orphanflag=" -orphan";
+		$orphanname=" orphaned ";
 	    } else {
-		print $testname.$orphanname." succeeded ";
-		$failed=0
-		}
-	    
-	    if($failed){
-		print "\n";
-	    } else {
-		# Run the crosstest
-		$cmd = "OMP_NUM_THREADS=".$numthreads."; ./crosstest_".$testname."> crosstest_".$testname.".out";
+		$orphanflag="";
+		$orphanname="";
+	    }	    
+	    $failed = "-";
+	    $crossresult = "-";
+	    if( ($i==0) || (($orphanedtest==0) && !($norphan)) ){
+		$cmd="./ompts_parser.pl ".$template." -test".$orphanflag;
+		system($cmd);
+		$cmd="./ompts_parser.pl ".$template." -crosstest".$orphanflag;
+		system($cmd);
+
+		# Compile:
+		$cmd="make test_".$testname." >> compile.log";
+		system($cmd);
+		$cmd="make crosstest_".$testname." >> compile.log";
+		system($cmd);
+
+		# Run the tests:
+		$cmd = "OMP_NUM_THREADS=".$numthreads."; ./test_".$testname."> test_".$testname.".out";
 		$exit_status = system($cmd);
 		if ($exit_status){
-		    $crossresult = $exit_status >> 8;
-		    print " and was verified\n";
+		    print $testname.$orphanname." failed !!! " ;
+		    $failed = $exit_status >> 8;
+		    $failed=0
 		} else {
-		    $crossresult=0;
-		    print " but might just have been lucky\n";
+		    print $testname.$orphanname." succeeded ";
+		    $failed=1
 		}
+
+		if(!$failed){
+		    print "\n";
+		} else {
+		    # Run the crosstest
+		    $cmd = "OMP_NUM_THREADS=".$numthreads."; ./crosstest_".$testname."> crosstest_".$testname.".out";
+		    $exit_status = system($cmd);
+		    if ($exit_status){
+			$crossresult = $exit_status >> 8;
+			print " and was verified\n";
+			$crossresult=1;
+		    } else {
+			$crossresult=0;
+			print " but might just have been lucky\n";
+		    }
+		}
+
+		# clean up
+		$cmd = "rm test_".$testname." crosstest_".$testname;
+		system($cmd);
 	    }
-	    
-            # clean up
-	    $cmd = "rm test_".$testname." crosstest_".$testname;
-	    system($cmd);
+	    print RESULTS "$failed\t$crossresult\t";
 	}
     }
-}
+    print RESULTS "\n";
+} # end of outer loop
+close(RESULTS);
+close(TEST);
