1fc69b71bad5264c9a26fc903e204a8bdc2f4562
Zhenying Liu <zliu@cs.uh.edu>
2005-10-10 23:32:01 +0000

    rewrite test for schedule static with chunk size. by Liao

--- OpenMPvalidation/SRC/C/TESTS/for_schedule_static.c
+++ OpenMPvalidation/SRC/C/TESTS/for_schedule_static.c
@@ -1,3 +1,9 @@
+/*
+*
+* For static scheduling we check whether the chunks have the requested size,
+* with the legal exception of the last chunk. 
+* Modified by Chunhua Liao
+*/
 #include <stdio.h>
 #include <omp.h>
 #include <unistd.h>
@@ -6,11 +12,7 @@
 #include "omp_testsuite.h"
 #include "omp_my_sleep.h"
 
-#define NUMBER_OF_THREADS 10
 #define CFSMAX_SIZE 1000
-#define CFDMAX_SIZE 1000000
-#define MAX_TIME 5
-#define SLEEPTIME 0.5
 
 
 
@@ -20,15 +22,12 @@ check_for_schedule_static (FILE * logFile)
   const int chunk_size = 7;
   int threads;
   int tids[CFSMAX_SIZE];
-  int i, lasttid;
-  int maxiter;
-  int notout = 1;
-  int counter = 0;
-  int tmp_count = 1;
-  int lastthreadsstarttid = -1;
+  int i;
   int result = 0;
+  int ii = 0;
+  int tid;
 
-#pragma omp parallel shared(tids,counter)
+#pragma omp parallel 
   {				/* begin of parallel */
 #pragma omp single
     {
@@ -45,89 +44,38 @@ check_for_schedule_static (FILE * logFile)
   else
     {
       fprintf (logFile,
-	       "Using an internal count of %d\nUsing a specified chunksize of %d\n",
-	       CFSMAX_SIZE, chunk_size);
-      tids[CFSMAX_SIZE - 1] = -1;	/* setting endflag */
-#pragma omp parallel shared(tids)
+       "Using an internal count of %d\nUsing a specified chunksize of %d\n",
+       CFSMAX_SIZE, chunk_size);
+
+#pragma omp parallel shared(tids) private(tid)
       {				/* begin of parallel */
-	int count = 0;
-	int tid;
 	tid = omp_get_thread_num ();
 
-#pragma omp for nowait schedule(static,chunk_size)
-	for (i = 0; i < CFSMAX_SIZE - 1; ++i)
+#pragma omp for schedule(static,chunk_size)
+	for (i = 0; i < CFSMAX_SIZE - 1; i++)
 	  {
-	    count = 0;
-#pragma omp flush(maxiter)
-	    if (i > maxiter)
-	      {
-#pragma omp critical
-		{
-		  maxiter = i;
-		}
-	      }
-	    /*printf("thread %d sleeping",tid); */
-	    while (notout && (count < MAX_TIME) && (maxiter == i))
-	      {
-#pragma omp flush(maxiter,notout)
-		my_sleep (SLEEPTIME);
-		count += SLEEPTIME;
-	      }
-	    /*printf("thread %d awake",tid); */
 	    tids[i] = tid;
 	  }
-
-	notout = 0;
-#pragma omp flush(maxiter,notout)
       }				/* end of parallel */
-
-
-
-      lasttid = tids[0];
-      tmp_count = 0;
-
-      for (i = 1; i < CFSMAX_SIZE; ++i)
+/*
+printf("debug---------------------\n");
+for (i=0;i<CFSMAX_SIZE -1;i++)
+  printf("%d ",tids[i]);
+printf("End of debug---------------------\n");
+*/
+      for (i = 0; i < CFSMAX_SIZE; ++i)
 	{
-	  if (tids[i] == lasttid)
+	ii= (i/chunk_size) % threads; /*round-robin for static chunk*/
+	  if (tids[i] != ii)
 	    {
-	      tmp_count++;
-	    }
-	  if (tids[i] == lasttid + 1 || tids[i] == 0)
-	    {
-	      if (tmp_count < chunk_size && lastthreadsstarttid < 0)
-		{
-		  lastthreadsstarttid = tids[i - 1];
-		}
-	      if (tmp_count > chunk_size)
-		{
-		  result++;
-		  fprintf (logFile, "Thread got %d chunks instead of %d",
-			   tmp_count, chunk_size);
-		  tmp_count = 1;
-		  lasttid = tids[i];
-		}
-	      if (lastthreadsstarttid == tids[i])
-		{
-		  result++;
-		  fprintf (logFile,
-			   "Error: Thread got chunk after he got his endchunk");
-		}
-	    }
-	  if (tids[i] >= threads)
-	    {
+             result++;
 	      fprintf (logFile,
-		       "Found thread with a threadnumber greater than the number of existing threads");
-	    }
-	  else
-	    {
-	      fprintf (logFile,
-		       "The next chunk was got by thread number %d instead of thread number %d",
-		       tids[i], (lasttid == threads) ? lasttid + 1 : 0);
-	      result++;
+		       "Iteration %d should be assigned to %d instead of %d\n",
+                        i,ii,tids[i]);
 	    }
 	}
       /*printf("Alles OK beim Test von schedule(static)\n"); */
-      return result;
+      return (result==0);
     }
 }
 
@@ -137,15 +85,12 @@ crosscheck_for_schedule_static (FILE * logFile)
   const int chunk_size = 7;
   int threads;
   int tids[CFSMAX_SIZE];
-  int i, lasttid;
-  int maxiter;
-  int notout = 1;
-  int counter = 0;
-  int tmp_count = 1;
-  int lastthreadsstarttid = -1;
+  int i;
   int result = 0;
+  int ii = 0;
+  int tid;
 
-#pragma omp parallel shared(tids,counter)
+#pragma omp parallel 
   {				/* begin of parallel */
 #pragma omp single
     {
@@ -162,81 +107,39 @@ crosscheck_for_schedule_static (FILE * logFile)
   else
     {
       fprintf (logFile,
-	       "Using an internal count of %d\nUsing a specified chunksize of %d\n",
-	       CFSMAX_SIZE, chunk_size);
-      tids[CFSMAX_SIZE - 1] = -1;	/* setting endflag */
-#pragma omp parallel shared(tids)
+       "Using an internal count of %d\nUsing a specified chunksize of %d\n",
+       CFSMAX_SIZE, chunk_size);
+
+#pragma omp parallel shared(tids) private(tid)
       {				/* begin of parallel */
-	int count = 0;
-	int tid;
 	tid = omp_get_thread_num ();
 
-#pragma omp for nowait
-	for (i = 0; i < CFSMAX_SIZE - 1; ++i)
+#pragma omp for 
+	for (i = 0; i < CFSMAX_SIZE - 1; i++)
 	  {
-	    count = 0;
-#pragma omp flush(maxiter)
-	    if (i > maxiter)
-	      {
-#pragma omp critical
-		{
-		  maxiter = i;
-		}
-	      }
-	    /*printf("thread %d sleeping",tid); */
-	    while (notout && (count < MAX_TIME) && (maxiter == i))
-	      {
-#pragma omp flush(maxiter,notout)
-		my_sleep (SLEEPTIME);
-		count += SLEEPTIME;
-	      }
-	    /*printf("thread %d awake",tid); */
 	    tids[i] = tid;
 	  }
-
-	notout = 0;
-#pragma omp flush(maxiter,notout)
       }				/* end of parallel */
-
-
-      lasttid = tids[0];
-      tmp_count = 0;
-
-      for (i = 1; i < CFSMAX_SIZE; ++i)
+/*
+printf("debug---------------------\n");
+for (i=0;i<CFSMAX_SIZE -1;i++)
+  printf("%d ",tids[i]);
+printf("End of debug---------------------\n");
+*/
+      for (i = 0; i < CFSMAX_SIZE; ++i)
 	{
-	  if (tids[i] == lasttid)
+	ii= (i/chunk_size) % threads; /*round-robin for static chunk*/
+	  if (tids[i] != ii)
 	    {
-	      tmp_count++;
-	    }
-	  if (tids[i] == lasttid + 1 || tids[i] == 0)
-	    {
-	      if (tmp_count < chunk_size && lastthreadsstarttid < 0)
-		{
-		  lastthreadsstarttid = tids[i - 1];
-		}
-	      if (tmp_count > chunk_size)
-		{
-		  result++;
-		  /*fprintf(logFile,"Thread got %d chunks instead of %d",tmp_count,chunk_size); */
-		  tmp_count = 1;
-		  lasttid = tids[i];
-		}
-	      if (lastthreadsstarttid == tids[i])
-		{
-		  result++;
-		  /*fprintf(logFile,"Error: Thread got chunk after he got his endchunk"); */
-		}
-	    }
-	  if (tids[i] >= threads)
-	    {
-	      /*fprintf(logFile,"Found thread with a threadnumber greater than the number of existing threads"); */
-	    }
-	  else
-	    {
-	      /*fprintf(logFile,"The next chunk was got by thread number %d instead of thread number %d",tids[i],(lasttid==threads)?lasttid+1:0); */
-	      result++;
+             result++;
+	      fprintf (logFile,
+		       "Iteration %d should be assigned to %d instead of %d\n",
+                        i,ii,tids[i]);
 	    }
 	}
-      return result;
+      /*printf("Alles OK beim Test von schedule(static)\n"); */
+      return (result==0);
     }
+
 }
+
