b4c04b26cd3b9baa6df0058ab3b78294b32bbb86
Christoph Niethammer <niethammer@hlrs.de>
2006-02-13 13:53:15 +0000

    The first fortran template

--- OpenMPvalidation/SRC/F90/TEST/do_private.f
+++ OpenMPvalidation/SRC/F90/TEST/do_private.f
@@ -1,11 +1,12 @@
-!********************************************************************
-! Functions: chk_do_private
-!********************************************************************
-
-        subroutine do_some_work()
+<ompts:test>
+<ompts:testdescription></ompts:testdescription>
+<ompts:version>2.0</ompts:version>
+<ompts:directive>omp do private</ompts:directive>
+<ompts:dependences>omp parallel private, omp flush, omp critical</ompts:dependences>
+<ompts:testcode>
+      subroutine do_some_work()
         implicit none
         real i
-!Yi Wen modified here, f90
         intrinsic sqrt
         double precision sum
         include "omp_testsuite.f"
@@ -13,73 +14,48 @@
         do i=0.0, LOOPCOUNT-1,1.0
           sum = sum + sqrt(i)
         end do
-        end
+      end
 
-      integer function chk_do_private()
-      implicit none
-      integer sum, sum0, sum1, known_sum, i
-      include "omp_testsuite.f"
-      sum = 0
-      sum0 = 0
-      sum1 = 0
+      integer function <ompts:testcode:functionname>do_private</ompts:testcode:functionname>()
+        implicit none
+        integer sum, known_sum
+<ompts:orphan:vars>
+        integer sum0, sum1, i
+        COMMON /orphvars/ sum0, sum1, i
+</ompts:orphan:vars>        
+
+        include "omp_testsuite.f"
+        sum = 0
+        sum0 = 0
+        sum1 = 0
 !$omp parallel private(sum1)
-      sum0 = 0
-      sum1 = 0
-!$omp do private(sum0) schedule(static,1)
-!Yi Wen modified: 05032004
-      do i=1, LOOPCOUNT
-         sum0=sum1
+        sum0 = 0
+        sum1 = 0
+<ompts:orphan>
+!$omp do <ompts:check>private(sum0)</ompts:check> schedule(static,1)
+        do i=1, LOOPCOUNT
+           sum0 = sum1
 !$omp flush
-        sum0 = sum0 + i
-        call do_some_work()
+          sum0 = sum0 + i
+          call do_some_work()
 !$omp flush
-!        print *, sum0
-        sum1 = sum0
-      end do
+!          print *, sum0
+          sum1 = sum0
+        end do
 !$omp end do
+</ompts:orphan>
 !$omp critical
-      sum = sum + sum1
+        sum = sum + sum1
 !$omp end critical
 !$omp end parallel
 
-      known_sum = (LOOPCOUNT*(LOOPCOUNT+1))/2
+        known_sum = (LOOPCOUNT*(LOOPCOUNT+1))/2
 !        print *, "sum:", sum, "known_sum", known_sum
-      if ( known_sum .eq. sum) then
-        chk_do_private = 1
-      else
-        chk_do_private = 0
-      end if
+        if ( known_sum .eq. sum) then
+           <testfunctionname></testfunctionname> = 1
+        else
+           <testfunctionname></testfunctionname> = 0
+        end if
       end
-
-      integer function crschk_do_private()
-      implicit none
-      integer sum, sum0, sum1, known_sum, i
-      include "omp_testsuite.f"
-      sum = 0
-      sum0 = 0
-      sum1 = 0
-!$omp parallel private(sum1)
-      sum0 = 0
-      sum1 = 0
-!$omp do schedule(static,1)
-      do i=1,LOOPCOUNT
-        sum0 = sum1
-!$omp flush
-        sum0 = sum0 + i
-        call do_some_work()
-!$omp flush
-        sum1 = sum0
-      end do
-!$omp end do
-!$omp critical
-      sum = sum + sum1
-!$omp end critical
-!$omp end parallel
-      known_sum = (LOOPCOUNT*(LOOPCOUNT+1))/2
-      if ( known_sum .eq. sum ) then
-        crschk_do_private = 1
-      else
-        crschk_do_private = 0
-      endif
-      end
-
+</ompts:testcode>
+</ompts:test>
