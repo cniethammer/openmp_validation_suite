5c70d6919eb45711926583db4cce09583ebd60e1
Christoph Niethammer <niethammer@hlrs.de>
2006-02-13 13:56:50 +0000

    fortran template

--- OpenMPvalidation/SRC/F90/TEST/omp_copyin.f
+++ OpenMPvalidation/SRC/F90/TEST/omp_copyin.f
@@ -1,62 +1,43 @@
-!********************************************************************
-! Functions: chk_omp_copyin
-!********************************************************************
-
-        integer function chk_omp_copyin()
-        implicit none
-        integer sum, known_sum, i
-!Yi Wen at 05042004 modified to add "save" for sum1 and
-!commented the below common block
-        integer, save::sum1
+<ompts:test>
+<ompts:testdescription>Test which checks the omp parallel copyin directive.</   ompts:testdescription>
+<ompts:ompversion>2.0</ompts:ompversion>
+<ompts:directive>omp parallel copyin</ompts:directive>
+<ompts:dependences>omp critical,omp threadprivate</ompts:dependences>
+<ompts:testcode>
+      INTEGER FUNCTION <ompts:testcode:functionname>omp_copyin</ompts:testcode:functionname>()
+        IMPLICIT NONE
+        INTEGER known_sum
+		<ompts:orphan:vars>
+! Edit c. Niethammer: removed save statement from variable sum1 because of problems with
+! the common block used for the orphaned test.
+!        INTEGER, SAVE::sum1
+        INTEGER sum1
+        INTEGER sum, i
+        COMMON /orphvars/ sum1, sum, i
+		</ompts:orphan:vars>
 !       common/csum1/ sum1
 !!!!!!!$omp threadprivate(/csum1/)
 !$omp threadprivate(sum1)
         sum = 0
         sum1 = 0
-!$omp parallel copyin(sum1)
+		<ompts:orphan>
+!$omp parallel <ompts:check>copyin(sum1)</ompts:check>
 !        print *,"sum1",sum1
 !$omp do
-        do i=1, 999
+        DO i=1, 999
           sum1 = sum1 + i
-        end do
+        END DO
 !$omp critical
         sum = sum + sum1
 !$omp end critical
 !$omp end parallel
+		</ompts:orphan>
         known_sum = 999*1000/2
-        if ( known_sum .eq. sum ) then
-           chk_omp_copyin = 1
-        else
-           chk_omp_copyin = 0
-        end if
-        end
-
-        integer function crschk_omp_copyin()
-        implicit none
-        integer sum, known_sum, i
-!Yi Wen at 05042004, same modification
-        integer, save::crosssum1
-!       common/ccrosssum1/ crosssum1
-!!!!!!!!$omp threadprivate(/ccrosssum1/)
-!$omp threadprivate(crosssum1)
-        sum = 0
-        crosssum1 = 789
-!$omp parallel
-!       print *,"cs",crosssum1
-!$omp do
-        do i=1, 999
-          crosssum1 = crosssum1 + i
-        end do
-!$omp end do
-!$omp critical
-        sum = sum + crosssum1
-!$omp end critical
-!$omp end parallel
-        known_sum = 999*1000/2
-        if ( known_sum .eq. sum ) then
-            crschk_omp_copyin = 1
-        else
-            crschk_omp_copyin = 0
-        end if
-        end
-
+        IF ( known_sum .EQ. sum ) THEN
+           <testfunctionname></testfunctionname> = 1
+        ELSE
+           <testfunctionname></testfunctionname> = 0
+        END IF
+      END
+</ompts:testcode>
+</ompts:test>
