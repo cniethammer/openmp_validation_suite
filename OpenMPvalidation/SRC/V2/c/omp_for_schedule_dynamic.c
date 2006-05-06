1168890f9b69268c50a5a7e77d9a6c8445a9274a
Christoph Niethammer <niethammer@hlrs.de>
2006-05-06 09:11:27 +0000

    Modified evaluation section, added comments and a lot of debugging information. (use -DVERBOSE)

--- OpenMPvalidation/SRC/V2/c/omp_for_schedule_dynamic.c
+++ OpenMPvalidation/SRC/V2/c/omp_for_schedule_dynamic.c
@@ -12,11 +12,10 @@
 #include "omp_my_sleep.h"
 
 #define NUMBER_OF_THREADS 10
-#define CFSMAX_SIZE 1000
-#define CFDMAX_SIZE 1000000
-#define MAX_TIME 5
+#define CFDMAX_SIZE 50000
 
-const int chunk_size = 10;
+
+const int chunk_size = 7;
 
 int <ompts:testcode:functionname>omp_for_schedule_dynamic</ompts:testcode:functionname> (FILE * logFile)
 {
@@ -26,10 +25,10 @@ int <ompts:testcode:functionname>omp_for_schedule_dynamic</ompts:testcode:functi
 
     int i;
     int tidsArray[CFDMAX_SIZE];
-    int count = 0;
+    int count;
     int tmp_count = 0;
     int *tmp;
-    int result = 1;
+    int result = 0;
 
     tids = tidsArray;
 
@@ -48,58 +47,74 @@ int <ompts:testcode:functionname>omp_for_schedule_dynamic</ompts:testcode:functi
 		   * dynamic distribution of the chunks. */
 		  if (j == 1) {
 			  my_sleep(SLEEPTIME);
+#ifdef VERBOSE
+			  fprintf(logFile, "Thread %d waited %lf seconds\n", tid, SLEEPTIME);
+#endif
 		  }
 		  tids[j] = tid;
 	    }	/* end of for */
 	</ompts:orphan>
 	}	/* end of orphaned block */
     }	/* end of parallel */
-
-    for (i = 0; i < CFDMAX_SIZE - 1; ++i){
-	if(tids[i] != tids[i + 1])
-	{
-	    count++;
+	
+	/* determining the number of assigned chunks and allocating the necessary 
+	 * memory for the evaluation */
+	count = 1;
+	for (i = 0; i < CFDMAX_SIZE - 1; ++i){
+	  if(tids[i] != tids[i + 1])
+		count++;
 	}
-    }
+#ifdef VERBOSE
+	fprintf (logFile, "%d chunks were assigned.\n", count);
+#endif
+    tmp = (int*) malloc(sizeof (int) * (count));
 
-    tmp = (int*) malloc(sizeof (int) * (count + 1));
+	/* write the chunksizes in the tmp array */ 
     tmp[0] = 1;
-
     for (i = 0; i < CFDMAX_SIZE - 1; ++i)
-    {
-	if (tmp_count > count) {
-	    printf("--------------------\nTestinternal Error: List too small!!!\n--------------------\n"); /* Error handling */
-	    break;
-	}
-	if (tids[i] != tids[i + 1]) {
-	    tmp_count++;
-	    tmp[tmp_count] = 1;
-	}
-	else {
-	    tmp[tmp_count]++;
-	}
-    }
-
-    /* is dynamic statement working? */
-
-    for(i = 0; i < count + 1; ++i)
-    {
-	if(tmp[i] != chunk_size)
 	{
-	    result += ((tmp[i] / chunk_size) - 1);
+	  /* First du some error handling */
+	  if (tmp_count >= count) {
+		fprintf(logFile,"Error: List for the evaluation of the results is too small.\n");
+		break;
+	  }
+	  /* Now check the chunks */
+	  if (tids[i] != tids[i + 1]) {
+		tmp_count++;
+		tmp[tmp_count] = 1;
+	  }
+	  else 
+		tmp[tmp_count]++;
 	}
-    }
-    /* for (i = 0; i < count + 1; ++i) printf("%d\t:=\t%d\n", i + 1, tmp[i]); */
-    if((tmp[0] != CFDMAX_SIZE) && (result > 1))
-    {
-	fprintf (logFile, "Seems to work. (Treads got %d times chunks \"twice\" by a total of %d chunks)\n", result, CFDMAX_SIZE / chunk_size); 
-	return (1);
-    }
-    else
-    {
-	fprintf (logFile, "Test negativ.\n");
-	return (0);
-    }
+#ifdef VERBOSE
+	fprintf(logFile, " Nr.:\tsize\n");
+    for (i = 0; i < count; i++) 
+	  fprintf(logFile, "%5d:\t%d\n", i + 1, tmp[i]);
+#endif
+
+	/* Evaluation of the results stored in the tmp array */
+    for(i = 0; i < count ; ++i)
+	{
+	  if(tmp[i] != chunk_size) {
+	    if (tmp[i] > chunk_size)
+		  result++;
+		else  {
+		  if (i == count - 1)
+			fprintf (logFile, "Last chunk had chunksize %d.\n", tmp[i]);
+		  else {
+			fprintf (logFile, "Error: Found chunk with chunksize %d (< %d) before the end.\n", tmp[i], chunk_size);
+			result = 0;
+			break;
+		  }
+		}
+	  }
+	}
+	if((tmp[0] != CFDMAX_SIZE) && result ) {
+	  fprintf (logFile, "Seems to work. (Treads got %d times chunks \"twice\" by a total of %d chunks)\n", result, CFDMAX_SIZE / chunk_size); 
+	  return result; 
+	}
+	else 
+	  return 0;
 }
 </ompts:testcode>
 </ompts:test>
