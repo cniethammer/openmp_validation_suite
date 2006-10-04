c3bd18cb98c570a0fe874f4cec27bcf7f9440ad8
Christoph Niethammer <niethammer@hlrs.de>
2006-10-04 17:00:08 +0000

    Modified the style of result presentation. Fixed minor bugs. Added some more documentation stuff.

--- OpenMPvalidation/SRC/V2/ompts_standaloneProc.c
+++ OpenMPvalidation/SRC/V2/ompts_standaloneProc.c
@@ -1,28 +1,30 @@
 int main()
 {
-	int i,result;
-	int failed=0, success=0;
-	int N=REPETITIONS;
-	static FILE * logFile;
-	static const char * logFileName = "c<ompts:crosscheck>cross</ompts:crosscheck><testfunctionname></testfunctionname>.log";
+	int i;			/* Loop index */
+	int result;		/* return value of the program */
+	int failed=0; 		/* Number of failed tests */
+	int success=0;		/* number of succeeded tests */
+	static FILE * logFile;	/* pointer onto the logfile */
+	static const char * logFileName = "c<ompts:crosscheck>cross</ompts:crosscheck><testfunctionname></testfunctionname>.log";	/* name of the logfile */
 
 
+	/* Open a new Logfile or overwrite the existing one. */
 	logFile = fopen(logFileName,"w+");
 
 	printf("######## OpenMP Validation Suite V %s ######\n", OMPTS_VERSION );
-	printf("## Repetitions: %3d                       ####\n",N);
+	printf("## Repetitions: %3d                       ####\n",REPETITIONS);
 	printf("## Loop Count : %6d                    ####\n",LOOPCOUNT);
 	printf("##############################################\n");
 	printf("Testing <directive></directive>\n\n");
 
-	fprintf(logFile,"######## OpenMP Validation Suite V OMPTS_VERSION ######\n");
-	fprintf(logFile,"## Repetitions: %3d                       ####\n",N);
+	fprintf(logFile,"######## OpenMP Validation Suite V %s ######\n", OMPTS_VERSION );
+	fprintf(logFile,"## Repetitions: %3d                       ####\n",REPETITIONS);
 	fprintf(logFile,"## Loop Count : %6d                    ####\n",LOOPCOUNT);
 	fprintf(logFile,"##############################################\n");
 	fprintf(logFile,"Testing <directive></directive>\n\n");
 
-	for(i=0;i<N;i++){
-		fprintf (logFile, "\n\n%d. run of <testfunctionname></testfunctionname> out of %d\n\n",i+1,N);
+	for ( i = 0; i < REPETITIONS; i++ ) {
+		fprintf (logFile, "\n\n%d. run of <testfunctionname></testfunctionname> out of %d\n\n",i+1,REPETITIONS);
 		if(<testfunctionname></testfunctionname>(logFile)){
 			fprintf(logFile,"Test succesfull.\n");
 			success++;
@@ -40,9 +42,10 @@ int main()
 		result=0;
 	}
 	else{
-		fprintf(logFile,"\nDirective failed the test %i times out of %i. %i were successful\n",failed,N,success);
-		printf("Directive failed the test %i times out of %i.\n%i test(s) were successful\n",failed,N,success);
-		result=failed;
+		fprintf(logFile,"\nDirective failed the test %i times out of %i. %i were successful\n",failed,REPETITIONS,success);
+		printf("Directive failed the test %i times out of %i.\n%i test(s) were successful\n",failed,REPETITIONS,success);
+		result = (int) ((failed / REPETITIONS ) * 100 );
 	}
+	printf ("Result: %i\n", result);
 	return result;
 }
--- OpenMPvalidation/SRC/V2/runtest.pl
+++ OpenMPvalidation/SRC/V2/runtest.pl
@@ -8,33 +8,36 @@
 # Use make to compile the test
 #
 # Options:
-# -norphan				do not generate orphaned test versions
+# -norphan			do not generate orphaned test versions
 # -maxthread=COUNT		run tests with a threadnumber from 2 up to COUNT
-# -norun				only compile, no run
+# -norun			only compile, no run
 # -nocompile			do not compile, only run
 # -lang=LANGUAGE		specify explicit the language (c or fortran)
-# -d=DIR				specify the directory containing the templates
-#						default is templates
+# -d=DIR			specify the directory containing the templates explicitly
 # -results=FILE			specify the name of the file containing the results
-# --help				print help
+# -logfile=FILE			specify the name of the file for global log messages
+# --help			print help
 #
 
-$results = "results.txt";
-$dir = "";
+$results 	= "results.txt";
+$logfilename 	= "ompts.log";
+$dir 		= "";
 
 # Using Getopt::long to extract the programm options
 use Getopt::Long;
 
 # Getting given options
 GetOptions("--help" => \$help, 
-			"-norphan" => \$norphan,
-			"-maxthread=i" => \$maxthread, 
-			"-norun" => \$norun, 
-    		"-nocompile" => \$nocompile, 
-			"-lang=s" => \$language,
-			"-d=s" => \$dir, 
-			"-results=s" => \$results, 
-			"-f!");
+      "-norphan" => \$norphan,
+      "-minthreads=i" => \$minthreads, 
+      "-maxthreads=i" => \$maxthreads, 
+      "-norun" => \$norun, 
+      "-nocompile" => \$nocompile, 
+      "-lang=s" => \$language,
+      "-d=s" => \$dir, 
+      "-results=s" => \$results, 
+      "-logfile=s" => \$logfilename, 
+      "-f!");
 $testfile = $ARGV[0];
 
 if($help){
@@ -46,46 +49,52 @@ compiles the source using make and runs finaly all tests.
 
 Options:
  -norphan            do not generate orphaned test versions
- -maxthread=COUNT    run tests with a threadnumber from 2 up to COUNT
+ -minthreads=COUNT   run tests with a minimum of COUNT>2 threads 
+ -maxthreads=COUNT   run tests with a threadnumber from 2 up to COUNT
  -norun              only compile, no run
  -nocompile          do not compile, only run
  -lang=LANGUAGE      specify explicit the language (c or fortran)
  -d=DIR              specify the directory containing the templates
             	     default is templates
  -results=FILE       specify the name of the file containing the results
+ -logfile=FILE	     specify the name of the file for global log messages
  --help              print help 
 ";
     print $helptext;
     exit 0;
 }
 
+# Set minthreads if it was not specified with the program arguments
+if(!($minthreads) || $minthreads < 2){ $minthreads = 2; }
 # Set maxthread if it was not specified with the program arguments
-if(!($maxthread)){ $maxthread = 2; }
+if(!($maxthreads) || $maxthreads < $minthreads ){ $maxthreads = $minthreads; }
 # Checking if given testfile exists
-die "The specified testlist does not exist." if(!(-e $testfile));
+if(!(-e $testfile)) { die "The specified testlist does not exist."; }
 # Checking if language was specified
-#if($language eq "c") { $extension = "tpl"; }
 if($language eq "c") { $extension = "c"; }
 elsif($language eq "fortran" or $language eq "f") { $language = "f"; $extension = "f"; }
 else { die "You must specify an valid language!\n"; }
 
 # printing some Information
-print "Running tests with maximal $maxthread threads.\n";
-print "Using testlist $testfile for input.\n";
-print "Using dir $dir to search testtemplates.\n";
-print "Compiling tests for language $language.\n";
+$message = "Running tests with mimimal $minthreads threads.
+Running tests with maximal $maxthreads threads.
+Using testlist $testfile for input.
+Using dir $dir to search for testtemplates.
+Compiling tests for language $language.\n";
+
+print $message;
+
 # printing information in logfile
-$logfilename = "ompts.log";
 open(GLOBALLOG,">$logfilename") or die "Error: Could not create  $logfilename\n";
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime time;
 $year += 1900;
-print GLOBALLOG "Running tests with maximal $maxthread threads.\n";
-print GLOBALLOG "Using testlist $testfile for input.\n";
-print GLOBALLOG "Using dir $dir to search testtemplates.\n";
-print GLOBALLOG "Compiling tests for language $language.\n";
-print GLOBALLOG "Test startet on $mday.$mon.$year at $hour:$min\n\n";
+print GLOBALLOG "Test startet on $mday.$mon.$year at $hour:$min:$sec\n\n";
+print GLOBALLOG $message;
+close(GLOBALLOG);
 $cmd = "make print_compile_options >> $logfilename";
 system($cmd);
+open(GLOBALLOG,">>$logfilename") or die "Error: Could not create  $logfilename\n";
+print GLOBALLOG "\n\nStarting tests:\n\n";
 
 # generating an up to date header file using the ompts_makeHeader.pl script if compiling for c
 if ($language == "") {
@@ -102,11 +111,11 @@ open(RESULTS,">$results") or die "Error: Could not create  $results\n";
 
 
 print RESULTS "\\ Number of Threads\t";
-for($j=2; $j <= $maxthread; $j++){
+for($j=$minthreads; $j <= $maxthreads; $j++){
     print RESULTS "$j\t\t\t\t";
 }
 print RESULTS "\nTested Directive";
-for($j=2; $j <= $maxthread; $j++){
+for($j=$minthreads; $j <= $maxthreads; $j++){
     print RESULTS "\tt\tct\tot\toct";
 }
 print RESULTS "\n";
@@ -116,81 +125,136 @@ print RESULTS "\n";
 # the tested directive. It follows the result of the test, crosstest, orphaned
 # test and orphaned crosstest.
 TEST: while(<TEST>){
-    if (/^\s*#/) {next TEST;}
-	if (/^\s*$/) {next TEST;}
-    $testname = $_;
-    chomp($testname);
-    print RESULTS "$testname\t";
+# filter comment and blank lines
+	 if (/^\s*#/) {next TEST;}
+	 if (/^\s*$/) {next TEST;}
+	 $testname = $_;
+	 chomp($testname);
+	 $template = $dir."/".$testname.".".$extension;
 
-    for($numthreads = 2; $numthreads <= $maxthread; $numthreads++){
-	for($i=0; $i<2; $i++){
-	    # Create templates:
-	    $template = $dir."/".$testname.".".$extension;
+	 print RESULTS "$testname\t";
+	 print "\nTesting for $testname\n";
+	 print GLOBALLOG "Testing for $testname\n";
 
-	    $cmd="grep ompts:orphan ".$template." > /dev/null";
-	    $orphanedtest=system($cmd);
-	    if( ($i==1) && ($orphanedtest==0) && !($norphan) ){
-		$orphanflag=" -orphan";
-		$orphanname="orphaned";
-	    } else {
-		$orphanflag="";
-		$orphanname="";
-	    }	    
-	    $failed = "-";
-	    $crossresult = "-";
-	    if( ($i==0) || (($orphanedtest==0) && !($norphan)) ){
-		$cmd="./ompts_parser.pl ".$template." -test".$orphanflag." -lang=".$language;
-		system($cmd);
-		$cmd="./ompts_parser.pl ".$template." -crosstest".$orphanflag." -lang=".$language;
-		system($cmd);
+	 $cmd = "grep ompts:orphan ".$template." > /dev/null";
+	 $_ = system($cmd);
+	 $orphanedtest = 0;
+	 if ($_ == 0) { 
+	    print "+ Test is orphanable\n"; 
+	    if (!($noorphan)) { $orphanedtest = 1; }
+	    else 	      { $orphanedtest = 0; }
+	 }
 
-		# Compile:
-		if(!$nocompile){
-		    print "Creating source out of templates for $testname... \n";
-		    $cmd="make ".$language.$orphanname."test_".$testname." >> compile.log";
-		    system($cmd);
-		    $cmd="make ".$language.$orphanname."ctest_".$testname." >> compile.log";
-		    system($cmd);
-		}
-	    
-		# Run the tests:
-		if(!$norun){
-		    print "Running test using $numthreads threads ... ";
-		    $cmd = "OMP_NUM_THREADS=".$numthreads."; export OMP_NUM_THREADS; ./".$language.$orphanname."test_".$testname."> ".$language.$orphanname."test_".$testname.".out";
-		    $exit_status = system($cmd);
-		    if ($exit_status){
-			print $testname.$orphanname." failed !!! " ;
-			$failed = $exit_status >> 8;
-			$failed=0
-		    } else {
-			print $testname.$orphanname." succeeded ";
-			$failed=1
-		    }
+         # start compiling and running the test for the specified number of threads
+	 for($numthreads = $minthreads; $numthreads <= $maxthreads; $numthreads++){
+	    print GLOBALLOG "Testing with $numthreads threads:\n";
+	    for($i=0; $i<2; $i++){
 
-		    if(!$failed){
-			print "\n";
-		    } else {
-			# Run the crosstest
-			$cmd = "OMP_NUM_THREADS=".$numthreads."; export OMP_NUM_THREADS; ./".$language.$orphanname."ctest_".$testname."> ".$language.$orphanname."crosstest_".$testname.".out";
-			$exit_status = system($cmd);
-			if ($exit_status){
-			    $crossresult = $exit_status >> 8;
-			    print " and was verified\n";
-			    $crossresult=1;
-			} else {
-			    $crossresult=0;
-			    print " but might just have been lucky\n";
+	       $success = "-";
+	       $crossresult = "-";
+
+               # Create templates:
+	       if( ($i == 1) && $orphanedtest ){
+		  $orphanflag=" -orphan";
+		  $orphanname="orphaned";
+	          print GLOBALLOG " (orphanmode) ";
+	       } else {
+		  $orphanflag="";
+		  $orphanname="";
+	          print GLOBALLOG " (normalmode) ";
+	       }	    
+
+	       if( ($i==0) || $orphanedtest ){
+		  if(!$nocompile){
+                     # prepare sourcecode:
+		     print "Creating source out of templates ....";
+		     $cmd="./ompts_parser.pl $template -test $orphanflag -lang=$language";
+		     system($cmd);
+		     print "....";
+		     $cmd="./ompts_parser.pl $template -crosstest $orphanflag -lang=$language";
+		     system($cmd);
+		     print " successful\n";
+                     # Compile:
+		     print "Compiling sources .......";
+		     $cmd="make ".$language.$orphanname."test_".$testname." >> compile.log";
+		     $compile1 = system($cmd);
+		     print ".......";
+		     $cmd="make ".$language.$orphanname."ctest_".$testname." >> compile.log";
+		     $compile2 = system($cmd);
+		     if (!$compile1 && !$compile2) {
+			print "......... successful\n";
+		     }
+		     else {
+			print "......... failed\n";
+			printf GLOBALLOG "Error: Compilation failed\n";
+		     }
+		  }
+
+                  # Run the tests:
+		  if(!$norun){
+		     $exec_name = $language.$orphanname."test_".$testname;
+                     # First check, if executable exists
+		     if (-e "$exec_name") { 
+			if ($orphanname eq "orphaned") {
+			   print "Running orphaned test using $numthreads threads ... ";
 			}
-		    }
-		    # clean up
-		    # $cmd = "rm test_".$testname." crosstest_".$testname;
-		    # system($cmd);
-		}
+			else {
+			   print "Running test using $numthreads threads ............ ";
+			}
+
+			$cmd = "OMP_NUM_THREADS=$numthreads; export OMP_NUM_THREADS; ./$exec_name > $exec_name.out";
+			$exit_status = system($cmd);
+			$failed = $exit_status >> 8;
+			$success = 100 - $failed;
+
+			if ($failed > 0){
+# print $testname.$orphanname." failed $failed\% of the tests.!" ;
+			   print "failed $failed\% of the tests!" ;
+			   print GLOBALLOG "Error: $failed\% ot the tests failed!\n";
+			} else {
+# print $testname.$orphanname." succeeded!";
+			   print "succeeded";
+			}
+
+			if($failed > 0){
+			   print "\n";
+			} else {
+# Run the crosstest if available
+			   $crossexec_name = $language.$orphanname."ctest_".$testname;
+			   if (-e "$crossexec_name") { 
+			      $cmd = "OMP_NUM_THREADS=$numthreads; export OMP_NUM_THREADS; ./$crossexec_name > $crossexec_name.out";
+			      $exit_status = system($cmd);
+			      if ($exit_status){
+				 $crossresult = $exit_status >> 8;
+				 print " and was verified with a certainty of $crossresult\%\n";
+				 printf GLOBALLOG "Succeeded  and was verified with a certainty of $crossresult\%\n";
+			      } else {
+				 $crossresult=0;
+				 print " but might just have been lucky (all crosstests failed)\n";
+				 printf GLOBALLOG "Succeeded but might just have been lucky (all crosstests failed)\n";
+			      }
+			   }
+			   else {
+			      $crossresult = 0;
+			   }
+			}
+		     }
+		     else {
+			$success = 0;
+		     }
+# clean up
+# $cmd = "rm $exec_name $crossexec_name";
+# system($cmd);
+		  }
+	       }
+	       print RESULTS "$success\t$crossresult\t";
 	    }
-	    print RESULTS $failed."\t".$crossresult."\t";
-	}
-    }
-    print RESULTS "\n";
-} # end of outer loop
-close(RESULTS);
-close(TEST);
+	 }
+	 print RESULTS "\n";
+      } # end of outer loop
+    close(RESULTS);
+    close(TEST);
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime time;
+    $year += 1900;
+    print GLOBALLOG "\nTest ended on $mday.$mon.$year at $hour:$min:$sec\n\n";
+    close(GLOBALLOG);
