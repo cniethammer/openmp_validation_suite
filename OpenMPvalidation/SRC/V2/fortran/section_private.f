cb665f5b1d7cf94129ae813a3ee9b101db9c634f
Christoph Niethammer <niethammer@hlrs.de>
2006-12-01 10:30:29 +0000

    Template

--- OpenMPvalidation/SRC/V2/fortran/section_private.f
+++ OpenMPvalidation/SRC/V2/fortran/section_private.f
@@ -1,87 +1,48 @@
-!********************************************************************
-! Functions: chk_section_private
-!********************************************************************
-
-        integer function chk_section_private()
-        implicit none
-        integer sum, sum0, known_sum, i
+<ompts:test>
+<ompts:testdescription>Test which checks the parallel section private clause.</ompts:testdescription>
+<ompts:ompversion>2.0</ompts:ompversion>
+<ompts:directive>omp parallel section private</ompts:directive>
+<ompts:dependences>omp critical</ompts:dependences>
+<ompts:testcode>
+      INTEGER FUNCTION <ompts:testcode:functionname>section_private</ompts:testcode:functionname>()
+        IMPLICIT NONE
+        INTEGER sum, sum0, known_sum, i
         sum = 7
         sum0 = 0
 !$omp parallel
-!$omp sections private(sum0,i)
+!$omp sections <ompts:check>private(sum0,i)</ompts:check><ompts:crosscheck>private(i)</ompts:crosscheck>
 !$omp section
         sum0 = 0
-        do i=1, 399
+        DO i=1, 399
           sum0 = sum0 + i
-        end do
+        END DO
 !$omp critical
         sum = sum + sum0
 !$omp end critical
 !$omp section
         sum0 = 0
-        do i=400, 699
+        DO i=400, 699
           sum0 = sum0 + i
-        end do
+        END DO
 !$omp critical
         sum = sum + sum0
 !$omp end critical
 !$omp section
         sum0 = 0
-        do i=700, 999
+        DO i=700, 999
           sum0 = sum0 + i
-        end do
+        END DO
 !$omp critical
         sum = sum + sum0
 !$omp end critical
 !$omp end sections
 !$omp end parallel
         known_sum = (999*1000)/2+7
-        if ( known_sum .eq. sum) then
-          chk_section_private = 1
-        else
-          chk_section_private = 0
-        end if
-        end
-!************************************************************
-        integer function crschk_section_private()
-        implicit none
-        integer sum, sum0, known_sum, i
-        sum = 7
-        sum0 = 0
-!$omp parallel
-!$omp sections private(i)
-!$omp section
-        sum0 =0
-        do i=1, 399
-          sum0 = sum0 + i
-        end do
-!$omp critical
-        sum = sum + sum0
-!$omp end critical
-!$omp section
-        sum0 =0
-        do i=400, 699
-          sum0 = sum0 + i
-        end do
-!$omp critical
-        sum = sum + sum0
-!$omp end critical
-!$omp section
-        sum0 =0
-        do i=700, 999
-          sum0 = sum0 + i
-        end do
-!$omp critical
-        sum = sum + sum0
-!$omp end critical
-!$omp end sections
-!$omp end parallel
-        known_sum=(999*1000)/2+7
-        if ( known_sum .eq. sum ) then
-           crschk_section_private = 1
-        else
-           crschk_section_private = 0
-        end if
-        end
-
-
+        IF ( known_sum .EQ. sum) THEN
+          <testfunctionname></testfunctionname> = 1
+        ELSE
+          <testfunctionname></testfunctionname> = 0
+        END IF
+      END
+</ompts:testcode>
+</ompts:test>
