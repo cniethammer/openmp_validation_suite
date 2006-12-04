1c22a992dc676ee7efabbcc3244016ea3524fb0f
Christoph Niethammer <niethammer@hlrs.de>
2006-12-04 19:40:58 +0000

    Generated templates out of existing test.

--- OpenMPvalidation/SRC/V2/fortran/par_section_firstprivate.f
+++ OpenMPvalidation/SRC/V2/fortran/par_section_firstprivate.f
@@ -1,56 +1,33 @@
-!********************************************************************
-! Functions: chk_par_section_firstprivate
-!********************************************************************
-
-	integer function chk_par_section_firstprivate()
-        implicit none
-	integer sum, sum0, known_sum
-	sum = 7
-	sum0 = 11
-!$omp parallel sections firstprivate(sum0)
+<ompts:test>
+<ompts:testdescription>Test which checks the omp parallel sections firstprivate clause.</ompts:testdescription>
+<ompts:ompversion>2.0</ompts:ompversion>
+<ompts:directive>omp parallel sections firstprivate</ompts:directive>
+<ompts:dependences>omp critical</ompts:dependences>
+<ompts:testcode>
+      INTEGER FUNCTION <ompts:testcode:functionname>par_section_firstprivate</ompts:testcode:functionname>()
+        IMPLICIT NONE
+        INTEGER sum, sum0, known_sum
+        sum = 7
+        sum0 = 11
+!$omp parallel sections
+!<ompts:check>firstprivate(sum0)</ompts:check><ompts:crosscheck>private(sum0)</ompts:crosscheck>
 !$omp section
 !$omp critical 
-	sum = sum + sum0
-!$omp end critical
-!$omp critical
         sum = sum + sum0
 !$omp end critical
 !$omp critical
         sum = sum + sum0
 !$omp end critical
-!$omp end parallel sections
-	known_sum = 11*3 + 7
-	if ( known_sum .eq. sum ) then
-	  chk_par_section_firstprivate = 1
-	else
-	  chk_par_section_firstprivate = 0
-	end if
-	end 
-
-	integer function crschk_par_section_firstprivate()
-        implicit none
-	integer sum, sum0, known_sum
-	sum = 7
-	sum0 = 11
-!$omp parallel sections private(sum0)
-!$omp section
-!$omp critical
-	sum = sum + sum0
-!$omp end critical
-!$omp section
-!$omp critical
-        sum = sum + sum0
-!$omp end critical
-!$omp section
 !$omp critical
         sum = sum + sum0
 !$omp end critical
 !$omp end parallel sections
-	known_sum = 11*3 + 7
-	if ( known_sum .eq. sum ) then
-	  crschk_par_section_firstprivate = 1
-	else
-	  crschk_par_section_firstprivate = 0
-	end if
-	end
-
+        known_sum = 11*3 + 7
+        IF ( known_sum .EQ. sum ) THEN
+          <testfunctionname></testfunctionname> = 1
+        ELSE
+          <testfunctionname></testfunctionname> = 0
+        END IF
+      END 
+</ompts:testcode>
+</ompts:test>
