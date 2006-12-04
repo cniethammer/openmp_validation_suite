5138e1b3d1b3e84cc81edc637349d503dbe8dfcc
Christoph Niethammer <niethammer@hlrs.de>
2006-12-04 19:52:50 +0000

    Made template out of existing test.

--- OpenMPvalidation/SRC/V2/fortran/section_firstprivate.f
+++ OpenMPvalidation/SRC/V2/fortran/section_firstprivate.f
@@ -1,13 +1,15 @@
-!********************************************************************
-! Functions: chk_section_firstprivate
-!********************************************************************
-        integer function chk_section_firstprivate()
-        implicit none
-        integer sum, sum0, known_sum
+<ompts:test>
+<ompts:testdescription>Test which checks the omp section firstprivate directive by adding a variable which is defined before the parallel region.</ompts:testdescription>
+<ompts:ompversion>2.0</ompts:ompversion>
+<ompts:directive>omp firstprivate</ompts:directive>
+<ompts:testcode>
+      INTEGER FUNCTION <ompts:testcode:functionname>section_firstprivate</ompts:testcode:functionname>()
+        IMPLICIT NONE
+        INTEGER sum, sum0, known_sum
         sum = 7
         sum0 = 11
 !$omp parallel
-!$omp sections firstprivate(sum0)
+!$omp sections <ompts:check>firstprivate(sum0)</ompts:check><ompts:crosscheck>private(sum0)</ompts:crosscheck>
 !$omp section
 !$omp critical
         sum = sum + sum0
@@ -23,40 +25,11 @@
 !$omp end sections
 !$omp end parallel
         known_sum = 11*3+7
-        if ( known_sum .eq. sum) then
-          chk_section_firstprivate = 1
-        else
-          chk_section_firstprivate = 0
-        end if
-        end
-
-        integer function crschk_section_firstprivate()
-        implicit none
-        integer sum, sum0, known_sum
-        sum = 7
-        sum0 = 11
-!$omp parallel
-!$omp sections private(sum0)
-!$omp section
-!$omp critical
-        sum = sum + sum0
-!$omp end critical
-!$omp section
-!$omp critical
-        sum = sum + sum0
-!$omp end critical
-!$omp section
-!$omp critical
-        sum = sum + sum0
-!$omp end critical
-!$omp end sections
-!$omp end parallel
-        known_sum=11*3+7
-        if ( known_sum .eq. sum) then
-          crschk_section_firstprivate = 1
-        else
-          crschk_section_firstprivate = 0
-        end if
-        end
-
-
+        IF ( known_sum .EQ. sum) THEN
+          <testfunctionname></testfunctionname> = 1
+        ELSE
+          <testfunctionname></testfunctionname> = 0
+        END IF
+      END
+</ompts:testcode>
+</ompts:test>
