f666c2210892527e006f5e3cffcba5aa6d2c71cd
Christoph Niethammer <niethammer@hlrs.de>
2006-12-04 19:17:56 +0000

    Fixed bug and generated template. Unfortunately the test can pass again if schedule static divided the work into equal parts for each thread.

--- OpenMPvalidation/SRC/V2/fortran/do_schedule_guided.f
+++ OpenMPvalidation/SRC/V2/fortran/do_schedule_guided.f
@@ -1,58 +1,82 @@
-!********************************************************************
-! Functions: chk_do_schedule_guided
-! this version is buggy. I'll fix it in the next version.
-! by Zhenying Liu of U. of Houston.
-!********************************************************************
+<ompts:test>
+<ompts:testdescription>Test which checks the guided option of the omp do schedule directive.</ompts:testdescription>
+<ompts:ompversion>2.0</ompts:ompversion>
+<ompts:directive>omp do schedule(guided)</ompts:directive>
+<ompts:dependences>omp flush,omp do nowait,omp critical,omp single</ompts:dependences>
+<ompts:testcode>
+  ! TODO:
+  ! C. Niethammer:
+  !       Find check to decide if the test was run as schedule(static) because
+  !       this also can pass the test if the work is devided into thread-counts 
+      INTEGER FUNCTION <ompts:testcode:functionname>do_schedule_guided</ompts:testcode:functionname>(logfile)
+        IMPLICIT NONE
+        INTEGER omp_get_thread_num,omp_get_num_threads
+        CHARACTER*20 logfile
+        INTEGER threads
+        INTEGER tmp_count
+        INTEGER, allocatable :: tmp(:)
+        INTEGER ii, flag
+        INTEGER result
+        INTEGER expected
+        INTEGER openwork
+        DOUBLE PRECISION c
 
-        integer function chk_do_schedule_guided(logfile)
-        implicit none
-        integer CFSMAX_SIZE, MAX_TIME
-        integer omp_get_thread_num,omp_get_num_threads
-        character*20 logfile
-        double precision SLEEP_TIME
-        integer chunk_size
-        integer threads
-        integer tmp_count, count
-        integer, allocatable :: tmp(:)
-        integer i, ii, flag, notout, maxiter
-        integer result
-        double precision SLEEPTIME
+                <ompts:orphan:vars>
+        INTEGER i
+        INTEGER tid
+        INTEGER count
+
+        INTEGER SLEEPTIME 
+        INTEGER MAX_TIME
+        INTEGER CFSMAX_SIZE
+
 ! ... choose small iteration space for small sync. overhead
-        parameter (CFSMAX_SIZE = 150,SLEEP_TIME=1)
-        parameter (MAX_TIME = 5, chunk_size=7)
-        integer tids(0:CFSMAX_SIZE-1), tid
+        PARAMETER (SLEEPTIME = 1)
+        PARAMETER (MAX_TIME = 5)
+        PARAMETER (CFSMAX_SIZE = 150)
+
+        INTEGER notout
+        INTEGER maxiter
+        INTEGER tids(0:CFSMAX_SIZE-1)
+
+        COMMON /orphvars/ notout,maxiter,tids
+                </ompts:orphan:vars>
+
         result = 0
         notout = 1
-        flag = 0
         maxiter = 0
         count = 0
         tmp_count = 0
+        openwork = CFSMAX_SIZE
+
+! Determine the number of available threads
 !$omp parallel 
 !$omp single
         threads = omp_get_num_threads()
 !$omp end single
 !$omp end parallel
-        if ( threads .lt. 2) then
-         print *,"This test only works with at least two threads"
-         write(1,*) "This test only works with at least two threads"
-         chk_do_schedule_guided = 0
-         stop
-        end if
+        IF ( threads .LT. 2) THEN
+          PRINT *,"This test only works with at least two threads"
+          WRITE(1,*) "This test only works with at least two threads"
+          <testfunctionname></testfunctionname> = 0
+          STOP
+        END IF
 
 ! ... Now the real parallel work:
 ! ... Each thread will start immediately with the first chunk.
     
 !$omp parallel private(tid,count) shared(tids,maxiter)
-         tid = omp_get_thread_num()
-!$omp do schedule(guided,chunk_size)
-         do i = 0 , CFSMAX_SIZE-1
-           count = 0
+        tid = omp_get_thread_num()
+        <ompts:orphan>
+!$omp do <ompts:check>schedule(guided)</ompts:check>
+        DO i = 0 , CFSMAX_SIZE-1
+          count = 0
 !$omp flush(maxiter)
-           if ( i .gt. maxiter ) then                 
+          IF ( i .GT. maxiter ) THEN                 
 !$omp critical
-             maxiter = i
+            maxiter = i
 !$omp end critical
-           end if
+          END IF
 
 !..         if it is not our turn we wait
 !           a) until another thread executed an iteration
@@ -62,80 +86,83 @@
 !           c) timeout arrived 
 
 !$omp flush(maxiter,notout)
-
-           if ( notout .ge. 1 .and. count .lt. MAX_TIME
-     &          .and. maxiter .eq. i ) then
-               do while ( notout .ge. 1 .and. count .lt. MAX_TIME
-     &           .and. maxiter .eq. i )
-!                 call sleep(SLEEPTIME)
-                 call sleep(SLEEPTIME)
-                 count = count + SLEEPTIME
-               end do         
-           end if
+          IF ( notout .GE. 1 .AND. count .LT. MAX_TIME
+     &         .AND. maxiter .EQ. i ) THEN
+              DO WHILE ( notout .GE. 1 .AND. count .LT. MAX_TIME
+     &          .AND. maxiter .EQ. i )
+                CALL sleep(SLEEPTIME)
+                count = count + SLEEPTIME
+              END DO         
+          END IF
            tids(i) = tid
-          end do
-!$omp end do nowait ! bug 161,Liao
+        END DO
+!$omp end do nowait
+        </ompts:orphan>
 
-          notout = 0
-
+        notout = 0
 !$omp flush(notout)
+
 !$omp end parallel 
 
-       count = 0
+!*******************************************************!
+! evaluation of the values
+!*******************************************************!
+        count = 0
 
-       do i=0, CFSMAX_SIZE - 2
-         if ( tids(i) .ne. tids(i+1) ) then
-              count = count + 1
-         end if
-       end do
+        DO i=0, CFSMAX_SIZE - 2
+          IF ( tids(i) .NE. tids(i+1) ) THEN
+            count = count + 1
+          END IF
+        END DO
 
-       allocate( tmp(0:count)  )
-       tmp_count = 0
-       tmp(0) = 1
+        ALLOCATE( tmp(0:count)  )
+        tmp_count = 0
+        tmp(0) = 1
 ! ... calculate the chunksize for each dispatch
 
-       do i=0, CFSMAX_SIZE - 2
-         if ( tids(i) .eq. tids(i+1) ) then
+        DO i=0, CFSMAX_SIZE - 2
+          IF ( tids(i) .EQ. tids(i+1) ) THEN
            tmp(tmp_count) = tmp(tmp_count) + 1
-         else
-           tmp_count = tmp_count + 1
-           tmp(tmp_count) = 1
-         end if
-       end do
+          ELSE
+            tmp_count = tmp_count + 1
+            tmp(tmp_count) = 1
+          END IF
+        END DO
 
 ! ... Check if chunk sizes are decreased until equals to 
 ! ... the specified one, ignore the last dispatch 
 ! ... for possible smaller remainder
 
-       flag = 0
-       do i=0, count -2
-         if ( i .gt. 0 .and. tmp(i) .eq. tmp(i+1) ) then
-           flag = 1
-         end if
-! ... set flag to indicate the Chunk sizes should 
-! ... be the same from now on 
+! Determine the constant 
+        expected = openwork / threads
+        c = real(tmp(0)) / real(expected)
+        WRITE(1,*) "Found constant to be ", c
 
-         if ( flag .eq. 0 ) then
-             if ( tmp(i) .le. tmp(i+1) ) then
-                result = result + 1
-                write(1,*) "chunk size from ",i,"to ",i+1,
-     &                     "not decreased."
-             end if
-          else if ( tmp(i) .ne. tmp(i+1) ) then
-           result = result + 1
-           write(1,*) "chunk size not maintained."
-          end if
-         end do
+        DO i = 0, count - 2
+          WRITE(1,*) "open:", openwork, "size:", tmp(i)
+          IF (expected .GT. 1) THEN
+            expected = c * openwork / threads
+          END IF
 
-         if ( result .eq. 0 )then
-           chk_do_schedule_guided = 1 
-         else
-           chk_do_schedule_guided = 0
-         end if
-       end
+          IF (abs(tmp(i) - expected) .GE. 2 ) THEN
+            result = 1
+            WRITE(1,*) "Chunksize differed from expected ",
+     &         "value: ",tmp(i), "instead ", expected
+          END IF
 
-       integer function crschk_do_schedule_guided(logfile)
-        implicit none
-        character*20 logfile
-        crschk_do_schedule_guided = 0
-       end  
+          IF (i .GT. 0 .AND. (tmp(i-1) - tmp(i)) .LT. 0) THEN
+            WRITE(1,*) "Chunksize did not decrease: ", tmp(i),
+     &         "instead",tmp(i-1)
+          END IF
+
+          openwork = openwork - tmp(i)  
+        END DO
+
+        IF ( result .EQ. 0 ) THEN
+           <testfunctionname></testfunctionname> = 1 
+        ELSE
+           <testfunctionname></testfunctionname> = 0
+        END IF
+      END
+</ompts:testcode>
+</omtps:test>
