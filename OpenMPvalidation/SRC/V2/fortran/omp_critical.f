26289cae99fe73d981e4d20fedbf4ac03c964072
Christoph Niethammer <niethammer@hlrs.de>
2006-12-21 12:46:07 +0000

    Rewrote test using parallel sections. Introduced variables myi and myj because simple common vars i,j seem not to work.

--- OpenMPvalidation/SRC/V2/fortran/omp_critical.f
+++ OpenMPvalidation/SRC/V2/fortran/omp_critical.f
@@ -1,38 +1,51 @@
 <ompts:test>
-<ompts:testdescription>Test which checks the omp critical directive by counting up a variable in a parallelized loop within a critical section.
+<ompts:testdescription>Test which checks the omp critical directive by counting up a variable in a parallelized region within a critical section.
 
 </ompts:testdescription>
 <ompts:ompversion>2.0</ompts:ompversion>
 <ompts:directive>omp critical</ompts:directive>
 <ompts:testcode>
-! We use sleep in one thread to ensure the execution order of two threads
-! which is not a good practice in OpenMP programming. Need to be fixed
-! in future release.
-! Functions: check_omp_critical
-!********************************************************************
-
       INTEGER FUNCTION <ompts:testcode:functionname>omp_critical</ompts:testcode:functionname>()
         IMPLICIT NONE
         INTEGER known_sum
         <ompts:orphan:vars>
-        INTEGER i, sum
-        COMMON /orphvars/ sum, i
+        INTEGER i,j,myi,myj, sum
+        COMMON /orphvars/ sum, myi, myj
         </ompts:orphan:vars>
         sum = 0
+        myi = 0
+        myj = 500
 !$omp parallel
-!$omp do
-        DO i = 0 , 999
+!$omp sections
+
+!$omp section
+        DO i = 0 , 499
                 <ompts:orphan>
                 <ompts:check>
 !$omp critical
                 </ompts:check>
-           sum = sum + i
+           sum = sum + myi
+           myi = myi + 1
                 <ompts:check>
 !$omp end critical
                 </ompts:check>
                 </ompts:orphan>
         END DO
-!$omp end do
+
+!$omp section
+        DO j = 500 , 999
+                <ompts:orphan>
+                <ompts:check>
+!$omp critical
+                </ompts:check>
+           sum = sum + myj
+           myj = myj + 1
+                <ompts:check>
+!$omp end critical
+                </ompts:check>
+                </ompts:orphan>
+        END DO
+!$omp end sections
 !$omp end parallel
         known_sum = 999*1000/2
         IF ( known_sum .EQ. sum ) THEN
