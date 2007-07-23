b4afc0e31f7ea3b9940653008b68761c03b8831a
Chunhua Liao <chunhualiao@gmail.com>
2007-07-23 17:52:44 +0000

    Removed old bash test from bottom of file

--- OpenMPvalidation/SRC/V2/fortran/omp_testlock.f
+++ OpenMPvalidation/SRC/V2/fortran/omp_testlock.f
@@ -48,39 +48,3 @@
       END FUNCTION
 </ompts:testcode>
 </ompts:test>
-
-      integer function crschk_omp_testlock()
-      USE omp_lib
-                implicit none
-                integer result
-      integer nr_threads_in_single
-      INTEGER (KIND=OMP_LOCK_KIND) :: lck
-!      INTEGER lck
-      integer nr_iterations
-      integer i
-      include "omp_testsuite.f"
-      nr_iterations=0
-      nr_threads_in_single=0
-      call omp_init_lock(lck)
-
-      result=0
-!$omp parallel shared(lck,nr_threads_in_single,nr_iterations,result)
-!$omp do
-      do i=1,LOOPCOUNT
-!$omp flush
-      nr_threads_in_single=nr_threads_in_single+1
-!$omp flush
-      nr_iterations=nr_iterations+1
-      nr_threads_in_single=nr_threads_in_single-1
-      result=result+nr_threads_in_single
-      enddo
-!$omp end do
-!$omp end parallel
-      call omp_destroy_lock(lck)
-      if(result.eq.0 .and. nr_iterations .eq. LOOPCOUNT) then
-            crschk_omp_testlock=1
-      else
-            crschk_omp_testlock=0
-      endif
-      end
-
