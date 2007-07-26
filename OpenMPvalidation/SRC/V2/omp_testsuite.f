d05b6a4dbd75086d0bcaa651869d740a9ab53f1e
Chunhua Liao <chunhualiao@gmail.com>
2007-07-26 17:25:36 +0000

    Changed this file to include the LOOPCOUNT and N parameters for all fortran tests.

--- OpenMPvalidation/SRC/V2/omp_testsuite.f
+++ OpenMPvalidation/SRC/V2/omp_testsuite.f
@@ -1,2 +1,5 @@
-      integer LOOPCOUNT
-      parameter (LOOPCOUNT=1000)
+      INTEGER LOOPCOUNT !The number of times each loop is run
+      PARAMETER (LOOPCOUNT=1000) 
+
+      INTEGER N !The number of times each test is run
+      PARAMETER (N=20)
