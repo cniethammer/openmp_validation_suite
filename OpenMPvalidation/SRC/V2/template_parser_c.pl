80559f0417d95bea5481d5df1dacc9af0cdb60a6
Christoph Niethammer <niethammer@hlrs.de>
2008-06-20 11:46:11 +0000

    Workaround because of the use of some predefined constants in some orphaned variable declarations.

--- OpenMPvalidation/SRC/V2/template_parser_c.pl
+++ OpenMPvalidation/SRC/V2/template_parser_c.pl
@@ -83,7 +83,7 @@ if ($orphan) {
 # Deleting the former declarations of the variables in the orphan regions:
     ($code) = delete_tags('ompts:orphan:vars',($code));
 # Put all together:
-    $code = $orphvarsdef . $orphfuncsdefs . $code . $orphfuncs;
+    $code = "#include \"omp_testsuite.h\"\n" . $orphvarsdef . $orphfuncsdefs . $code . $orphfuncs;
     $testprefix .= "orph_";
 }
 
