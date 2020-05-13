b57e04a2e87e562d1fa2418ad103d02dd0c08645
Christoph Niethammer <niethammer@hlrs.de>
2020-05-13 21:37:04 +0200

    Add test for OpenMP task detach and omp_fulfill_event
    Test for OpenMP 5.0 task detach clause and related omp_fulfill_event
    function based on discussions with Joseph Schuchart and
    LLVM bug report https://bugs.llvm.org/show_bug.cgi?id=45904.

---
+++ src/c/omp_task_detach.c
@@ -0,0 +1,62 @@
+<ompts:test>
+<ompts:testdescription>Test which checks the detach clause of the omp task directive.</ompts:testdescription>
+<ompts:ompversion>5.0</ompts:ompversion>
+<ompts:directive>omp task detach,omp_fulfill_event</ompts:directive>
+<ompts:dependences>omp task, omp atomic</ompts:dependences>
+<ompts:testcode>
+#include <stdio.h>
+#include <math.h>
+#include "omp_testsuite.h"
+#include "omp_my_sleep.h"
+
+<ompts:orphan:vars>
+int result1 = 0;
+int result2 = 0;
+</ompts:orphan:vars>
+
+int <ompts:testcode:functionname>omp_task_detach</ompts:testcode:functionname>(FILE * logFile){
+#pragma omp parallel
+{
+#pragma omp master
+    {
+        <ompts:orphan>
+            omp_event_handle_t event;
+#pragma omp task shared(result1) <ompts:check>detach(event)</ompts:check>
+        {
+            <ompts:check>omp_fulfill_event(event);</ompts:check>
+            result1 = 1;
+        }
+        </ompts:orphan>
+#pragma omp taskwait
+
+    }
+
+
+#pragma omp master
+    {
+
+        <ompts:crosscheck>int dummy = 0;</ompts:crosscheck>
+        omp_event_handle_t event;
+#pragma omp task depend(out: result2) shared(result2) <ompts:check>detach(event)</ompts:check>
+        {
+            #pragma omp atomic
+            result2++;
+        }
+#pragma omp task shared(result2) <ompts:crosscheck>depend(out:dummy)</ompts:crosscheck>
+        {
+            #pragma omp atomic
+            result2++;
+            <ompts:check>omp_fulfill_event(event);</ompts:check>
+        }
+#pragma omp task depend(out:result2) shared(result2) <ompts:crosscheck>depend(in:dummy)</ompts:crosscheck>
+        {
+            result2 /= 2;
+        }
+#pragma omp taskwait
+    }
+}
+
+    return (result1 == 0) && (result2 == 1);
+}
+</ompts:testcode>
+</ompts:test>
